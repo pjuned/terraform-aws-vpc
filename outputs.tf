15fa1979e3eb18b8734a825b7c2c4e98da5b92a9
unknown <pjuned.it@gmail.com>
2024-11-28 02:59:06 +0530

    vpc

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "azs" {
+    value = data.aws_availability_zones.azs
+  
+}
+
+# output "elip" {
+#     value = aws_eip.eip
+  
+# }
---
+++ variables.tf
@@ -0,0 +1,115 @@
+variable "vpc_cidr" {
+    type = string
+    default = "10.0.0.0/16"
+  
+}
+
+variable "enable_dns_hostnames" {
+    type = bool
+    default = true
+  
+}
+
+variable "common_tags" {
+    type = map
+    default = {}
+  
+}
+
+variable "vpc_tags" {
+    type = map
+    default = {}
+}
+
+variable "project_name" {
+    type = string
+  
+}
+
+variable "environment" {
+    type = string
+  
+}
+
+variable "igw_tags" {
+
+    type = map
+    default = {}
+  
+}
+
+variable "public_subnets_cidr" {
+    type = list
+  validation {  
+    condition     = length(var.public_subnets_cidr) == 2  
+    error_message = "The list must contain exactly 2 items."  
+  }  
+}  
+
+variable "public_subnets_tags" {
+    default = {}
+  
+}
+
+variable "private_subnets_cidr" {
+    type = list
+  validation {  
+    condition     = length(var.private_subnets_cidr) == 2  
+    error_message = "The list must contain exactly 2 items."  
+  }  
+}  
+
+variable "private_subnets_tags" {
+    default = {}
+  
+}
+
+variable "database_subnets_cidr" {
+    type = list
+  validation {  
+    condition     = length(var.database_subnets_cidr) == 2  
+    error_message = "The list must contain exactly 2 items."  
+  }  
+}  
+
+variable "database_subnets_tags" {
+    default = {}
+  
+}
+
+variable "nat_gateway_tags" {
+    default = { }
+  
+}
+
+variable "public_route_table_tags" {
+    default = {}
+  
+}
+
+variable "private_route_table_tags" {
+    default = {}
+  
+}
+
+variable "database_route_table_tags" {
+    default = {}
+  
+}
+
+variable "acceptor_vpc_id" {
+    type = string
+    default = ""
+  
+}
+
+variable "vpc_peering_tags" {
+    default = {}
+  
+}
+
+variable "is_peering_required" {
+    type = bool
+    default = false
+  
+}
